cb121bfb3d94fcd3b8d3b9f0a567993575f9051e
koocooplatter <schwarz.kyle0@gmail.com>
2014-11-23 19:04:02 -0500

    static express + public folder
    Enables the CSS to work. Holla!

--- nodeExample.js
+++ nodeExample.js
@@ -1,48 +1,29 @@
 
+var express=require('express');
+var path = require('path');
 var app = require('express')();
 var http = require('http').Server(app);
-
-
-
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/workspace/rideclub_proto.html');
-  
-
+res.sendFile(__dirname + '/workspace/rideclub_proto.html');
 });
-
-//app.use("/", express.static(__dirname + '/workspace/css')); //need to redefine 'app' and 'express'
-
+app.use(express.static(path.join(__dirname, 'public'))); //need to redefine 'app' and 'express'
 http.listen(3000, function(){
-  console.log('Server running at http://127.0.0.1:3000/... http://3000 or something...');
+console.log('Server running at http://127.0.0.1:3000/... http://3000 or something...');
 });
-
-
-
-
-
-
-
-
-
-
 /*
 //twitter stuff. Will use l8r
 var util = require('util'),
-    twitter = require('twitter');
+twitter = require('twitter');
 var twit = new twitter({
-    consumer_key: 'KEY HERE',
-    consumer_secret: 'KEY HERE',
-    access_token_key: 'KEY HERE',
-    access_token_secret: 'KEY HERE'
+consumer_key: 'KEY HERE',
+consumer_secret: 'KEY HERE',
+access_token_key: 'KEY HERE',
+access_token_secret: 'KEY HERE'
 });
-
-
 twit.search('#tybg', function(data) {
-    console.log(util.inspect(data));
-	
+console.log(util.inspect(data));
 });
 //still having trouble finding kyle's tweetz. It does pull in popular hashtags though!
-
 app.get('/', twit.gatekeeper('/login'), routes.index);
 app.get('/login', routes.login);
 app.get('/twauth', twit.login());
