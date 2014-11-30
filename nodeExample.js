5cff7adbea5ef62a07b80444a1f1922804911ba3
helloimowen <helloimowens@gmail.com>
2014-11-30 00:59:58 -0500

    Trying to get twitter working with socket...
    help.

--- nodeExample.js
+++ nodeExample.js
@@ -1,31 +1,51 @@
 
-var express=require('express');
+var express = require('express');
 var path = require('path');
-var app = require('express')();
-var http = require('http').Server(app);
+var app = express();
+var http = require('http');
+var server = http.createServer(app)
+var io = require('socket.io').listen(server);
+
+
 app.get('/', function(req, res){
 res.sendFile(__dirname + '/workspace/rideclub_proto.html');
+
 });
-app.use(express.static(path.join(__dirname, 'public'))); //need to redefine 'app' and 'express'
-http.listen(3000, function(){
+
+app.use(express.static(path.join(__dirname, 'public')));
+server.listen(3000, function(){
 console.log('Server running at http://127.0.0.1:3000/... http://3000 or something...');
 });
+
+var flag = ['burlyrides', 'rides']
+
 
 //twitter stuff. Will use l8r
 var util = require('util'),
 twitter = require('twitter');
+
 var twit = new twitter({
-consumer_key: 'Rj3Gak3eLOGRB6SMcMSxTijMk',
-consumer_secret: 'hTUpVb4714tqSysiLrioKxF07a6evgUHgF22Xl5zUR4nM4ZukQ',
-access_token_key: '2881726402-5C0t2zmremomnb2hzQvsCXAncTbEYosbKNYO8yx',
-access_token_secret: '4hyDEDpL6tB4mJz4MY3SAC6WwjSRj9D2fPUNVUwyqhPvS'
+consumer_key: '',
+consumer_secret: '',
+access_token_key: '',
+access_token_secret: ''
 });
+
+io.sockets.on('connection', function (socket) {
+  console.log('woah! This actually worked!');
+
+
+ var stream = twit.stream('statuses/filter', { track: flag })
+
+  stream.on('tweet', function (tweet) {
+
+    io.sockets.emit('stream',tweet.text);
+ });
+}); 
+
+/*
 twit.search('#burlyrides', function(data) {
 console.log(util.inspect(data));
 });
-/*
-//still having trouble finding kyle's tweetz. It does pull in popular hashtags though!
-app.get('/', twit.gatekeeper('/login'), routes.index);
-app.get('/login', routes.login);
-app.get('/twauth', twit.login());
+
 */
