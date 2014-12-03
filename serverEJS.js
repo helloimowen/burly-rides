c73ba8be4ba621334d1ebe27092c1b948939e347
helloimowen <helloimowens@gmail.com>
2014-12-03 17:28:09 -0500

    burly-rides FINAL
    Final proof of concept.

--- serverEJS.js
+++ serverEJS.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var sentiment = require('sentiment');
 
 app.set('view engine', 'ejs');
-
-// use res.render to load up an ejs view file
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,24 +19,6 @@ access_token_key: '',
 access_token_secret: ''
 });
 
-
-
-
-/*
-app.get('/', function(req, res) {
-    var test = "I hate myself and I want to die.";
-
-
-    res.render('rideclub_proto', {
-		test: test
-	});
-
-
-});
-*/
-
-
-
 app.get('/', function(req, res) {
     twit.search('#tybg', function(data) {
         var stringTw = JSON.stringify(data);
@@ -50,16 +31,29 @@ app.get('/', function(req, res) {
         //console.log(util.inspect(JSONTw));
 
     
-    var test = "___I am tired. Help.";
-    
-  
-        
+    var test = "#burlyrides";
+
+	
+	
+	var fakeTwitter = [
+    { id: 1, name: "coolguy99", lat: 44.482870, lon: -73.216312, text: "I love everything. Cool stuff all around." },
+    { id: 2, name: "IhateBees", lat: 44.472496, lon: -73.218198, text: "I hate bees more than I hate everything else." },
+    { id: 3, name: "EverythingIsPain33", lat: 44.477730, lon: -73.195035, text: "I hate myself and I want to die." },
+];
+
+/*
+  	var sent1 = sentiment ("I love everything. Cool stuff all around.");
+	var sent2 = sentiment ("I hate bees more than I hate everything else.");
+	var sent3 = sentiment ("I hate myself and I want to die.");
+*/
     
 	res.render('rideclub_proto', {
         data : data,
         stringTw : stringTw,
         JSONTw : JSONTw,
-        test : test
+        test : test,
+		fakeTwitter : fakeTwitter
+
         });
    });
 
